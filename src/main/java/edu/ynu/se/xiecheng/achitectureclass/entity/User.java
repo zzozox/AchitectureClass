97f5a7465c5bbdca8ff24f952691aa814b933709
Cheng <xiecheng@ynu.edu.cn>
2022-12-07 22:46:18 +0800

    创建用户时MD5加密

--- src/main/java/edu/ynu/se/xiecheng/achitectureclass/entity/User.java
+++ src/main/java/edu/ynu/se/xiecheng/achitectureclass/entity/User.java
@@ -1,10 +1,10 @@
 package edu.ynu.se.xiecheng.achitectureclass.entity;
 
 import edu.ynu.se.xiecheng.achitectureclass.common.entity.LogicEntity;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Where;
+import org.springframework.util.DigestUtils;
 
 import javax.persistence.*;
 
@@ -20,6 +20,14 @@ public abstract class User extends LogicEntity {
 
     @Column
     protected String password;
+
+    /**
+     * 创建所有User时，密码经过md5加密
+     * @param psd
+     */
+    public void setPassword(String psd){
+        this.password = DigestUtils.md5DigestAsHex(psd.getBytes());
+    }
 
     @Column(updatable = false, insertable = false)
     protected Integer type;
